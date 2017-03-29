fe12fa198bf2664a097c9d073b74762cc16ecb09
BamBo <raksameth@gmail.com>
2017-03-29 18:01:41 +0700

    Override lifecycle method and show each of process on the logcat

--- MyApplication/app/src/main/java/com/raksa/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/raksa/myapplication/MainActivity.java
@@ -1,13 +1,51 @@
 package com.raksa.myapplication;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private final String TAG = "LifeCycle Message : ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG,"onCreate Method Loaded !");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG,"onStart Method Loaded !");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume Method Loaded !");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause Method Loaded !");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop Method Loaded !");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy Method Loaded !");
     }
 }
